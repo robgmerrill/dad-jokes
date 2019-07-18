604516ffc6ab0dc7193c831afeac65384dcfc256
Rob Merrill <robgmerrill@gmail.com>
2019-07-18 15:51:34 -0700

    Upvote and downvote functionality

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import Joke from "./Joke";
 // Import in axios for ajax request
 import axios from "axios";
+import uuid from 'uuid/v4';
 import './JokeList.css';
 
 class JokeList extends Component {
@@ -21,23 +23,31 @@ class JokeList extends Component {
       let res = await axios.get("https://icanhazdadjoke.com/", {
         headers: { Accept: "application/json" }
       });
-      jokes.push(res.data.joke);
+      jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
     }
     console.log(jokes);
     this.setState({ jokes: jokes });
+  }
+  handleVote(id, delta) {
+    this.setState(
+      st => ({
+        jokes: st.jokes.map(j =>
+          j.id === id ? { ...j, votes: j.votes + delta } : j)
+      })
+    )
   }
   render() {
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
           <h1 className="JokeList-title"><span>Dad</span> Jokes</h1>
-          <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+          <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' alt="Joy Emoji" />
           <button className="JokeList-getmore">New Jokes</button>
 
         </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(j => (
-            <div>{j}</div>
+            <Joke key={j.id} votes={j.votes} text={j.text} upvote={() => this.handleVote(j.id, 1)} downvote={() => this.handleVote(j.id, -1)} />
           ))}
         </div>
       </div>
