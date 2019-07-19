a8efd2beda727d30e22151e7a6bc2a6cf7feae88
Rob Merrill <robgmerrill@gmail.com>
2019-07-19 10:41:35 -0700

    Sort jokes by rating

--- src/JokeList.js
+++ src/JokeList.js
@@ -73,6 +73,7 @@ class JokeList extends Component {
         </div>
       );
     }
+    let jokes = this.state.jokes.sort((a, b) => b.votes - a.votes);
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
@@ -82,7 +83,7 @@ class JokeList extends Component {
 
         </div>
         <div className="JokeList-jokes">
-          {this.state.jokes.map(j => (
+          {jokes.map(j => (
             <Joke key={j.id} votes={j.votes} text={j.text} upvote={() => this.handleVote(j.id, 1)} downvote={() => this.handleVote(j.id, -1)} />
           ))}
         </div>
